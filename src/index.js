fb9db1577e062d306c194d7a8737d2bc9906b03c
Allen <1942813644@qq.com>
2022-02-25 12:45:00 +0800

    history route

--- src/index.js
+++ src/index.js
@@ -10,18 +10,22 @@ div4.innerHTML = '4'
 
 //路由表
 const routeTable = {
-  '1': div1,
-  '2': div2,
-  '3': div3,
-  '4': div4
+  '/1': div1,
+  '/2': div2,
+  '/3': div3,
+  '/4': div4
 }
 
 function route(container) {
   // 获取用户想去哪里
-  let number = window.location.hash.substring(1)
+  let number = window.location.pathname
+  console.log('number' + number)
 
+  if (number === '/') {
+    number = '/1'
+  }
 //默认路由
-  number = number || 1
+  number = number || '/1'
 // 获取界面
   let div = routeTable[number.toString()]
   if (!div) {
@@ -36,8 +40,21 @@ function route(container) {
   container.appendChild(div)
 }
 
+const allA = document.querySelectorAll('a.link')
+
+for (let a of allA) {
+  a.addEventListener('click', () => {
+    e.preventDefault()
+    const href = a.getAttribute('href')
+    window.history.pushState(null, 'page ${href}', 'href')
+    // 通知
+    onStateChange(href)
+  })
+}
+
 route(app)
-window.addEventListener('hashchange', () => {
-  console.log('hash 变了')
+
+function onStateChange() {
+  console.log('state 变了')
   route(app)
-})
+}
